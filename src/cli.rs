d9fac5b8a427ccd4a44f962337bcb03da4c298b1
Clayton <claytonwambu@gmail.com>
2022-12-14 20:44:52 +0000

    Fetch site html

--- src/cli.rs
+++ src/cli.rs
@@ -2,5 +2,5 @@ use clap::Parser;
 
 #[derive(Debug, Parser)]
 pub struct Args {
-    term: String,
+    pub term: String,
 }
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,16 @@ use cli::Args;
 
 const BASE_URL: &str = "https://glossary.infil.net/?t=";
 
-fn main() {
-    let args = Args::parse();
-    println!("{args:?}")
+#[tokio::main]
+async fn main() -> color_eyre::Result<()> {
+    let Args { term } = Args::parse();
+    let res = reqwest::Client::new()
+        .get(format!("{BASE_URL}{term}"))
+        .send()
+        .await?
+        .error_for_status()?;
+    let html = res.text().await?;
+    println!("{html}");
+
+    Ok(())
 }
