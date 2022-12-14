dbb8fa1ae75eaf82ac30ae03da649ef840100786
Clayton <claytonwambu@gmail.com>
2022-12-14 21:07:01 +0000

    Minor refactor

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,22 @@ mod cli;
 
 use clap::Parser;
 use cli::Args;
+use scraper::{Html, Selector};
 
 const BASE_URL: &str = "https://glossary.infil.net/?t=";
 
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+
     let Args { term } = Args::parse();
+    let url = format!("{BASE_URL}{term}");
     let res = reqwest::Client::new()
-        .get(format!("{BASE_URL}{term}"))
+        .get(&url)
         .send()
         .await?
         .error_for_status()?;
     let html = res.text().await?;
-    println!("{html}");
 
     Ok(())
 }
