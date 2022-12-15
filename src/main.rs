9d79367ce7cddd85ceeb2680b3178e9d2ecfaf68
Clayton <claytonwambu@gmail.com>
2022-12-15 19:12:17 +0000

    Get definitions from json instead of html

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,25 @@ mod cli;
 use clap::Parser;
 use cli::Args;
 use color_eyre::Result;
-use scraper::{Html, Selector};
+use serde::Deserialize;
 
 const BASE_URL: &str = "https://glossary.infil.net/?t=";
+const JSON_URL: &str = "https://glossary.infil.net/json/glossary.json";
 
 #[tokio::main]
 async fn main() -> Result<()> {
     color_eyre::install()?;
 
     let Args { term } = Args::parse();
-    let url = format!("{BASE_URL}{term}");
-    let res = reqwest::Client::new()
-        .get(&url)
-        .send()
-        .await?
-        .error_for_status()?;
-    let html = res.text().await?;
+    let res = reqwest::Client::new().get(JSON_URL).send().await?;
+    let json: Vec<Definition> = serde_json::from_str(&res.text().await?)?;
 
-    if let Some(def) = get_def(&html) {
-        println!("{def}");
-        println!("[See {url}]")
+    if let Some(def) = json
+        .iter()
+        .find(|d| d.term.to_lowercase() == term.to_lowercase())
+    {
+        println!("{}", def.def);
+        println!("{BASE_URL}{term}");
     } else {
         println!("No results.")
     }
@@ -30,15 +29,8 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
-fn get_def(html: &str) -> Option<String> {
-    let doc = Html::parse_document(html);
-    let selector = Selector::parse(r#"div[class="def"]"#).unwrap();
-
-    doc.select(&selector).next().map(|def| {
-        def.text()
-            .collect::<String>()
-            .split_whitespace()
-            .map(|s| s.to_string() + " ")
-            .collect()
-    })
+#[derive(Debug, Deserialize)]
+struct Definition {
+    term: String,
+    def: String,
 }
