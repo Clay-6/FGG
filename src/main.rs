85b0e2b1b5003696abc27364d174b09f501b27cc
Clayton <claytonwambu@gmail.com>
2022-12-17 13:02:13 +0000

    Move text cleaning to Definition::text

--- src/main.rs
+++ src/main.rs
@@ -21,21 +21,7 @@ async fn main() -> Result<()> {
         .iter()
         .find(|d| d.term.to_lowercase() == term.to_lowercase())
     {
-        let tmp = def.text();
-        let text = tmp.split_whitespace().collect::<Vec<_>>();
-        let mut txt = text.clone();
-        for (i, w) in txt
-            .iter_mut()
-            .enumerate()
-            .take_while(|(i, _)| *i < (text.len() - 1))
-        {
-            if *w.to_lowercase() == text[i + 1].to_lowercase() {
-                *w = "";
-            }
-        }
-        let text = txt.iter().map(|w| w.to_string() + " ").collect::<String>();
-        let double_space = Regex::new("  ").unwrap();
-        println!("{}", double_space.replace_all(&text, " "));
+        println!("{}", def.text());
         println!("[{BASE_URL}{term}]");
     } else {
         println!("No results.")
@@ -55,11 +41,26 @@ impl Definition {
         let opening_regex = Regex::new("!<'").unwrap();
         let closing_regex = Regex::new("'>").unwrap();
         let doubles_regex = Regex::new("[A-z]+','").unwrap();
+        let double_space = Regex::new("  ").unwrap();
         let tags_regex = Regex::new("<[^>]*>").unwrap();
 
         let mut tmp = opening_regex.replace_all(&self.def, "").to_string();
-        tmp = closing_regex.replace_all(&tmp, "").to_string();
-        tmp = doubles_regex.replace_all(&tmp, "").to_string();
-        tags_regex.replace_all(&tmp, "").to_string()
+        tmp = closing_regex.replace_all(&tmp, "").into();
+        tmp = doubles_regex.replace_all(&tmp, "").into();
+        tmp = tags_regex.replace_all(&tmp, "").into();
+
+        let text = tmp.split_whitespace().collect::<Vec<_>>();
+        let mut txt = text.clone();
+        for (i, w) in txt
+            .iter_mut()
+            .enumerate()
+            .take_while(|(i, _)| *i < (text.len() - 1))
+        {
+            if *w.to_lowercase() == text[i + 1].to_lowercase() {
+                *w = "";
+            }
+        }
+        let text = txt.iter().map(|s| s.to_string() + " ").collect::<String>();
+        double_space.replace_all(&text, " ").to_string()
     }
 }
